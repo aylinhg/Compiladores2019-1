505558179ca9dcf9073bed7bd17156b7ba0c53cf
aylinhg <aylin_hg@ciencias.unam.mx>
2018-10-25 19:32:26 -0500

    Se agregan funciones en el Visitante que permitan imprimir

--- Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/Visitor.java
+++ Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/Visitor.java
@@ -3,6 +3,35 @@ import ast.patron.compuesto.*;
 
 public interface Visitor
 {
-    public void visit(IntHoja n);
-    public void visit(Nodo n);
+    public void visit(IntHoja n); 
+    public void visit(Nodo n); 
+    public void visit(AndNB n); 
+    public void visit(AsignacionNB n); 
+    public void visit(BoolHoja n); 
+    public void visit(Compuesto n); 
+    public void visit(DiffNB n); 
+    public void visit(DivisionEnteraNB n); 
+    public void visit(DivisionNB n); 
+    public void visit(EqualsNB n); 
+    public void visit(GrNB n); 
+    public void visit(GrqNB n);
+    public void visit(Hoja n); 
+    public void visit(IdentifierHoja n); 
+    public void visit(LeNB n); 
+    public void visit(LeqNB n); 
+    public void visit(ModuloNB n); 
+    public void visit(MultiNB n); 
+    public void visit(NodoBinario n); 
+    public void visit(NodoIf n);
+    public void visit(NodoNot n); 
+    public void visit(NodoStmts n); 
+    public void visit(OrNB n); 
+    public void visit(PotenciaNB n);   
+    public void visit(PrintNB n); 
+    public void visit(RealHoja n);
+    public void visit(RestaNB n); 
+    public void visit(StringHoja n);
+    public void visit(SumaNB n);
+	public void visit(WhileNB n);
+
 }
--- Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/VisitorPrint.java
+++ Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/VisitorPrint.java
@@ -14,4 +14,125 @@ public class VisitorPrint implements Visitor
 
     }
 
+    public void visit(AndNB n){
+    	System.out.println("[AND]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(AsignacionNB n){
+    	System.out.println("[=]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(BoolHoja n){
+    	System.out.print("[Hoja Booleano]: " + n.getValor().bval);
+    }
+
+    public void visit(Compuesto n){
+    	for (Iterator i = n.getHijos().iterator(); i.hasNext(); ) {
+            Nodo hijo = (Nodo) i.next();
+            System.out.print("[");
+            hijo.accept(this);
+            System.out.println("]");
+        }
+    }
+
+    public void visit(DiffNB n){
+    	System.out.println("[!=]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(DivisionEnteraNB n){
+    	System.out.println("[//]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(DivisionNB n){
+    	System.out.println("[/]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(EqualsNB n){
+    	System.out.println("[==]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(GrNB n){
+    	System.out.println("[>]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(GrqNB n){
+    	System.out.println("[>=]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(Hoja n){
+		System.out.print("[Hoja]: "+ n.toString()); 	
+    }
+    
+    public void visit(IdentifierHoja n){
+    	System.out.print("[Hoja Identificador] id: "+ n.getNombre());
+    }
+
+    public void visit(LeNB n){
+    	System.out.println("[<]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
+    public void visit(LeqNB n){
+    	System.out.println("[<=]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+		System.out.println("]");
+    }
+
 }
