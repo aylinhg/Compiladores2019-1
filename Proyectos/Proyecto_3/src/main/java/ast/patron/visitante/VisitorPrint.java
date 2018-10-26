b3233e8c702bdabed9704b11c938fdbc81df97c9
javiertellezvieyra <javiertellezvieyra@ciencias.unam.mx>
2018-10-25 20:04:45 -0500

    Se completan funciones del Visitor que premiten imprimir :v

--- Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/VisitorPrint.java
+++ Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/VisitorPrint.java
@@ -135,4 +135,135 @@ public class VisitorPrint implements Visitor
 		System.out.println("]");
     }
 
+        public void visit(ModuloNB n){
+        System.out.println("[mod]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(MultiNB n){
+        System.out.println("[*]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(NodoBinario n){
+
+    }
+
+    public void visit(NodoIf n){
+        System.out.println("[if]");
+        System.out.println("[");
+        if(n.getPrimerHijo() != null){
+            n.getPrimerHijo().accept(this);
+        }
+        System.out.println("]");
+        System.out.println("[");
+        if(n.getHijos().getAll().get(1) != null){
+            n.getHijos().getAll().get(1).accept(this);
+        }
+        System.out.println("]");
+        if(n.getHijos().size() == 3){
+            System.out.println("[else]");
+            System.out.println("[");
+            if(n.getHijos().getAll().get(2) != null){
+                n.getHijos().getAll().get(2).accept(this);
+            }
+            System.out.println("]");
+        }
+    }
+    
+    public void visit(NodoNot n){
+        System.out.println("[!]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(NodoStmts n){
+
+    }
+
+    public void visit(OrNB n){
+        System.out.println("[OR]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(PotenciaNB n){
+        System.out.println("[^]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(PrintNB n){
+        System.out.println("[print]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        if(n.getUltimoHijo()!= null){
+            n.getUltimoHijo().accept(this);            
+        }        
+        System.out.println("]");
+    }
+
+    public void visit(RealHoja n){
+        System.out.print("[Hoja Real]: " + n.getValor().dval);
+    }
+
+    public void visit(RestaNB n){
+        System.out.println("[-]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        n.getUltimoHijo().accept(this);
+    }
+    
+    public void visit(StringHoja n){
+        System.out.print("[Hoja Cadena]: " + n.getValor().sval);
+    }
+
+    public void visit(SumaNB n){
+        System.out.println("[+]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
+
+    public void visit(Variable n){
+
+    }
+
+    public void visit(WhileNB n){
+        System.out.println("[while]");
+        System.out.print("[");
+        n.getPrimerHijo().accept(this);
+        System.out.print("]");
+        System.out.print("[");
+        n.getUltimoHijo().accept(this);
+        System.out.println("]");
+    }
 }
